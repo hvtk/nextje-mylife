1412bedaefd4ef7efb0cd0b6619935aa345e3a7c
hvtk <hvtk1968@gmail.com>
2023-07-15 09:02:56 +0200

    creating selection component

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,14 @@
+import styles from './page.module.css'
+
+export default function Page() {
+  return (
+    <main className={styles.main}>
+        <article>
+          <img src="/images/selections/family.jpg" alt="images represending your family" width="153" height="153">
+
+          </img>
+        </article>
+
+    </main>
+  )
+}
