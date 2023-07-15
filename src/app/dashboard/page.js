be10790cca39a4a3bc255e7a75af9da34d3fd435
hvtk <hvtk1968@gmail.com>
2023-07-15 10:34:00 +0200

    added selection name on image

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -4,11 +4,14 @@ export default function Page() {
   return (
     <main className={styles.main}>
         <article>
-          <img src="/images/selections/family.jpg" alt="images represending your family" width="153" height="153">
-
-          </img>
+          <img src="/images/selections/family.jpg" 
+               alt="images represending your family" 
+               width={153} 
+               height={153}
+               className={styles.img_selection}
+          />
+          <p className={styles.name_selection}> Family and Friends</p>
         </article>
-
     </main>
   )
 }
