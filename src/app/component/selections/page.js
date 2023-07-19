b9289b02c0985e6d27a18158b8aeb93f25dc3b96
hvtk <hvtk1968@gmail.com>
2023-07-19 15:13:48 +0200

    trying to use Next.js Image Component. Not fixed yet.

--- src/app/component/selections/page.js
+++ src/app/component/selections/page.js
@@ -1,8 +1,15 @@
-<article>
-    <img src="/images/selections/family.jpg" 
+import Image from 'next/image'
+import selectImageFamily from '../public/images/selections/family.jpg'
+
+export default function Page() {
+  return(
+    <div>
+      <Image
+         src={selectImageFamily} 
          alt="images represending your family" 
-         width={153} 
-         height={153}
-    />
-    <p> Family and Friends</p>
-</article>
+         sizes="100vw"
+      />
+      <p> Family and Friends</p>
+    </div>
+  )
+}
