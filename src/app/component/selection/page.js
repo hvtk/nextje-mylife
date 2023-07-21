f5a431c333e67d8ed131ca73d20aee96b236550f
hvtk <hvtk1968@gmail.com>
2023-07-21 12:03:27 +0200

    made component selection reusable with different prop values for src and alt.

--- src/app/component/selection/page.js
+++ src/app/component/selection/page.js
@@ -1,11 +1,10 @@
 import Image from 'next/image'
-import selectImageFamily from 'public/images/selections/family.jpg'
 
-export function Selection() {
+export function Selection({ src, alt }) {
   return (
     <Image
-      src={selectImageFamily} 
-      alt="images represending your family" 
+      src={src} 
+      alt={alt} 
       width={153} 
       height={153}
       style={{borderRadius: "7px"}}
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,12 +1,24 @@
 import styles from './dashboard.module.css'
 import {Selection} from '../component/selection/page'
+import selectImageWhereILive from 'public/images/selections/where i live.jpg'
+import selectImageFamily from 'public/images/selections/family.jpg'
 
 export default function Page() {
   return (
     <main className={styles.main}>
         <article>
-          <Selection />
+          <Selection 
+            src={selectImageFamily}
+            alt="image respresending my family and friends"
+          />
           <p className={styles.name_selection}> Family and Friends</p>
+        </article>  
+        <article>
+          <Selection
+            src={selectImageWhereILive} 
+            alt="images represending my place to live"
+          /> 
+          <p className={styles.name_selection}> Where I live</p>
         </article>
     </main>
   )
