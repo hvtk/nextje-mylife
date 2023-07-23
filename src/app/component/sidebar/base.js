36132e6ae0f8471fbfde70e93b797b41b0d9d4e7
hvtk <hvtk1968@gmail.com>
2023-07-23 19:29:31 +0200

    for the logo I have to use grid in stead of flex! Not ready yet!

--- src/app/component/sidebar/base.js
+++ src/app/component/sidebar/base.js
@@ -1,16 +1,28 @@
 import styles from './sidebarStyles.module.css'
-import { MylifeLogo } from './mylifeLogo'
+import { MylifeLogoFS } from './mylifeLogo'
 import { MylifeLogoTL } from './mylifeLogo'
+import { MylifeLogoTR } from './mylifeLogo'
+import { MylifeLogoBR } from './mylifeLogo'
+import { MylifeLogoBL } from './mylifeLogo'
 
 export function SidebarBase() {
   return (
     <div className={styles.divSidebar} >
           <div className={styles.divLogo} >
-            <div className={styles.mylifeLogFS} >
-                <MylifeLogo/>
+            <div className={styles.mylifeLogoFS} >
+                <MylifeLogoFS/>
             </div>
             <div className={styles.mylifeLogoTL} >
                 <MylifeLogoTL />
+            </div>
+            <div className={styles.mylifeLogoTR} >
+                <MylifeLogoTR />
+            </div>
+            <div className={styles.mylifeLogoBR} >
+                <MylifeLogoBR />
+            </div>
+            <div className={styles.mylifeLogoBL} >
+                <MylifeLogoBL />
             </div>
           </div>
     </div>
--- src/app/component/sidebar/mylifeLogo.js
+++ src/app/component/sidebar/mylifeLogo.js
@@ -1,8 +1,11 @@
 import Image from 'next/image'
 import MylifeLogoImageFill from 'public/images/mylife-logo/life.jpg'
 import MylifeLogoImageTopLeft from 'public/images/mylife-logo/home.jpg'
+import MylifeLogoImageTopRight from 'public/images/mylife-logo/sad.jpg'
+import MylifeLogoImageBottomRight from 'public/images/mylife-logo/working.jpg'
+import MylifeLogoImageBottomLeft from 'public/images/mylife-logo/happy.jpg'
 
-export function MylifeLogo() {
+export function MylifeLogoFS() {
   return (
     <Image
       src={MylifeLogoImageFill}
@@ -17,9 +20,42 @@ export function MylifeLogoTL() {
   return (
     <Image
       src={MylifeLogoImageTopLeft}
-      alt="image that fills the parent element"
+      alt="image on top left in the parent element"
       width={26}
       height={30}
+    />
+  )
+}
+
+export function MylifeLogoTR() {
+  return (
+    <Image
+      src={MylifeLogoImageTopRight}
+      alt="image on top right in the parent element"
+      width={24}
+      height={35}
+    />
+  )
+}
+
+export function MylifeLogoBR() {
+  return (
+    <Image
+      src={MylifeLogoImageBottomRight}
+      alt="image on bottom right in the parent element"
+      width={34}
+      height={30}
+    />
+  )
+}
+
+export function MylifeLogoBL() {
+  return (
+    <Image
+      src={MylifeLogoImageBottomLeft}
+      alt="image on bottom left in the parent element"
+      width={35}
+      height={35}
     />
   )
 }
