5f7e705925f757e9f21d673f9442822ade70c83e
hvtk <hvtk1968@gmail.com>
2023-07-26 14:57:49 +0200

    added icons for sidebar. not ready yet.

--- src/app/component/sidebar/base.js
+++ src/app/component/sidebar/base.js
@@ -1,12 +1,23 @@
+import Image from 'next/image'
 import styles from './sidebarStyles.module.css'
 import { MylifeLogo } from './mylifeLogo'
+import { SidebarList } from 'public/icons/sidebar/list.svg'
 
 
 export function SidebarBase() {
   return (
     <div className={styles.divSidebar} >
       <MylifeLogo /> 
+      <div className={styles.SidebarList}>
+      <Image
+          src={SidebarList}
+          alt="icon for list options/selection"
+          width={60}
+          height={60}
+        />
+        </div>
     </div>
   )
 }
+
 
