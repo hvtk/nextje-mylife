bb10787c728360f4f589155d944b2202057a956d
hvtk <hvtk1968@gmail.com>
2023-07-26 09:55:58 +0200

    Logo for sidebar is ready!

--- src/app/component/sidebar/base.js
+++ src/app/component/sidebar/base.js
@@ -1,30 +1,11 @@
 import styles from './sidebarStyles.module.css'
-import { MylifeLogoFS } from './mylifeLogo'
-import { MylifeLogoTL } from './mylifeLogo'
-import { MylifeLogoTR } from './mylifeLogo'
-import { MylifeLogoBR } from './mylifeLogo'
-import { MylifeLogoBL } from './mylifeLogo'
+import { MylifeLogo } from './mylifeLogo'
+
 
 export function SidebarBase() {
   return (
     <div className={styles.divSidebar} >
-          <div className={styles.divLogo} >
-            <div className={styles.mylifeLogoFS} >
-                <MylifeLogoFS/>
-            </div>
-            <div className={styles.mylifeLogoTL} >
-                <MylifeLogoTL />
-            </div>
-            <div className={styles.mylifeLogoTR} >
-                <MylifeLogoTR />
-            </div>
-            <div className={styles.mylifeLogoBR} >
-                <MylifeLogoBR />
-            </div>
-            <div className={styles.mylifeLogoBL} >
-                <MylifeLogoBL />
-            </div>
-          </div>
+      <MylifeLogo /> 
     </div>
   )
 }
--- src/app/component/sidebar/mylifeLogo.js
+++ src/app/component/sidebar/mylifeLogo.js
@@ -1,61 +1,57 @@
 import Image from 'next/image'
+import styles from './sidebarStyles.module.css'
 import MylifeLogoImageFill from 'public/images/mylife-logo/life.jpg'
 import MylifeLogoImageTopLeft from 'public/images/mylife-logo/home.jpg'
 import MylifeLogoImageTopRight from 'public/images/mylife-logo/sad.jpg'
 import MylifeLogoImageBottomRight from 'public/images/mylife-logo/working.jpg'
 import MylifeLogoImageBottomLeft from 'public/images/mylife-logo/happy.jpg'
 
-export function MylifeLogoFS() {
+export function MylifeLogo() {
   return (
-    <Image
-      src={MylifeLogoImageFill}
-      alt="image that fills the parent element"
-      width={80}
-      height={80}
-    />
-  )
-}
-
-export function MylifeLogoTL() {
-  return (
-    <Image
-      src={MylifeLogoImageTopLeft}
-      alt="image on top left in the parent element"
-      width={26}
-      height={30}
-    />
-  )
-}
-
-export function MylifeLogoTR() {
-  return (
-    <Image
-      src={MylifeLogoImageTopRight}
-      alt="image on top right in the parent element"
-      width={24}
-      height={35}
-    />
-  )
-}
-
-export function MylifeLogoBR() {
-  return (
-    <Image
-      src={MylifeLogoImageBottomRight}
-      alt="image on bottom right in the parent element"
-      width={34}
-      height={30}
-    />
-  )
-}
-
-export function MylifeLogoBL() {
-  return (
-    <Image
-      src={MylifeLogoImageBottomLeft}
-      alt="image on bottom left in the parent element"
-      width={35}
-      height={35}
-    />
+    <div className={styles.divLogo} >
+      <div>
+        <Image
+          src={MylifeLogoImageFill}
+          alt="image that fills the parent element"
+          placeholder='blur'
+          quality={100}
+          fill
+          sizes="100vw"
+        />
+      </div>
+      <div className={styles.mylifeLogoTL} >
+        <Image
+          src={MylifeLogoImageTopLeft}
+          alt="image on top left in the parent element"
+          width={25}
+          height={30}
+        />
+      </div>
+      <div className={styles.mylifeLogoTR} >
+        <Image
+          src={MylifeLogoImageTopRight}
+          alt="image on top right in the parent element"
+          width={23}
+          height={32}
+        />
+      </div>
+      <div className={styles.mylifeLogoBR} >
+        <Image
+          src={MylifeLogoImageBottomRight}
+          alt="image on bottom right in the parent element"
+          width={34}
+          height={30}
+        />
+      </div>
+      <div className={styles.mylifeLogoBL} >
+        <Image
+          src={MylifeLogoImageBottomLeft}
+          alt="image on bottom left in the parent element"
+          width={29}
+          height={34}
+        />
+      </div>
+    </div>
+   
   )
 }
